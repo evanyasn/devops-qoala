eb465d8ffb03d03ec7bd8c08cda6ac0f50e62425
root <root@mongodbrestore.c.getcraft-151305.internal>
2020-01-15 13:10:50 +0000

    finished test

--- create_s3_bucket.py
+++ create_s3_bucket.py
@@ -8,7 +8,7 @@ try:
         service_name="s3",
         aws_access_key_id="123",
         aws_secret_access_key="123",
-        endpoint_url="http://localhost:4572",
+        endpoint_url="http://localstack:4572",
     )
 
     bucket_test1 = s3_client.create_bucket(Bucket="test1")
